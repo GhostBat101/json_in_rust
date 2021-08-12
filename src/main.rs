17383be3f3296fa8c0a6b3fdf6cabc6d58203a51
GhostBat101 <kamruzzaman080@protonmai.com>
2021-08-12 15:25:16 +0600

    Adding struck and external crate.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+extern crate rustc_serialize;
+use rustc_serialize::json::{self,ToJson, Json};
+
+struct Dvd {
+    name: String,
+    year: u16,
+    cast: String,
+    length: u16,
+}
+
+
+
 fn main() {
     println!("Hello, world!");
 }
